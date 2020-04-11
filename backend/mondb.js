0cf1dd47da275390a0dce1fff0672c24a93a65f8
Ratan Kaliani <ratankaliani@berkeley.edu>
2020-04-11 02:08:18 -0400

    Getting unreadable JSON

--- backend/mondb.js
+++ backend/mondb.js
@@ -2,11 +2,16 @@ import Mongo from 'mongodb';
 const MongoClient = Mongo.MongoClient;
 const uri = "mongodb+srv://ratankaliani:micr0gr%21D@cluster0-iasb3.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
+
+client.connect(err => {
+  const db = client.db("Users");
+  const collection = db.collection("rights")
+  const object = collection.find({rate: 21})
+  console.log(object)
+//   console.log(collection);
+  // perform actions on the collection object
+  
+  
+  client.close();
+});
 export default { client };
-// client.connect(err => {
-//   const collection = client.db("Users").collection("Login/Password");
-//   // perform actions on the collection object
-//   const username = collection.find({username: "ratan"});
-//   console.log(username);
-//   client.close();
-// });
