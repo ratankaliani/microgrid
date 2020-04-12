c5b64d3f1cbdf48bcba7620720c50753ad9c221d
Ratan Kaliani <ratankaliani@berkeley.edu>
2020-04-11 21:22:13 -0400

    Client connecting.

--- backend/mondb.js
+++ backend/mondb.js
@@ -1,17 +1,20 @@
 import Mongo from 'mongodb';
 const MongoClient = Mongo.MongoClient;
-const uri = "mongodb+srv://ratankaliani:micr0gr%21D@cluster0-iasb3.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://ratankaliani:bL0cK%3Fp4rT%2A@cluster0-iasb3.mongodb.net/test?retryWrites=true&w=majority?authSource=admin";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 client.connect(err => {
-  const db = client.db("Users");
-  const collection = db.collection("rights")
-  const object = collection.find({rate: 21})
-  console.log(object)
-//   console.log(collection);
-  // perform actions on the collection object
-  
-  
+  if (err) throw err;
+  const dbo = client.db("Users");
+  var query = {};
+  dbo.collection("Login/Password").find(query).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    
+  });
   client.close();
 });
+
+
+
 export default { client };
