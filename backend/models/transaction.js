21ef7778880fe58184453735988fb1fe6e80a29a
Ratan Kaliani <ratankaliani@berkeley.edu>
2020-04-13 00:18:38 -0400

    removed babelrc

---
+++ backend/models/transaction.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+import uri from "../mondb.js"
+
+//Connect to mongoose
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userLoginSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  fromAddress: {
+    type: String,
+    require: true
+  },
+  toAddress: {
+    type: String,
+    require: true
+  },
+  price: {
+    type: Number,
+    default: '',
+    require: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  expiresAt: {
+      type: Date,
+      default: Date.now
+  }
+}, {
+  writeConcern: {
+    w: 'majority',
+    j: true,
+    wtimeout: 1000
+  }
+});
+
+export default mongoose.model('UserLogin', userLoginSchema)
