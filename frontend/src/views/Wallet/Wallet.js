3b699c7add43aa53b6cd67908fdcab42553cda75
Omkar Waingankar <omkar.waingankar@berkeley.edu>
2020-05-02 22:18:09 -0400

    maxBuy, minSell pulled from backend

--- frontend/src/views/Wallet/Wallet.js
+++ frontend/src/views/Wallet/Wallet.js
@@ -4,14 +4,24 @@ import React from 'react';
 import WalletView from "./WalletView.js"
 import {REACT_APP_BACKEND_URL} from "../../assets/constants.js";
 
+// import Mongo from 'mongodb';
+// import mongoose from 'mongoose';
+// import User from '../../tempModels/user.js';
+// const MongoClient = Mongo.MongoClient;
+// const uri = "mongodb+srv://ratankaliani:bL0cK%3Fp4rT%2A@cluster0-iasb3.mongodb.net/Microgrid?retryWrites=true&w=majority?authSource=admin";
+
+// mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
 export default class Wallet extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            user: null,
             loading: false,
-            user: undefined,
-            username: ""
+            username: "",
+            minSell: 0,
+            maxBuy: 0
         };
     }
 
@@ -29,43 +39,16 @@ export default class Wallet extends React.Component {
             }
         })
         .then(response => response.json())
-        .then(user => this.setState({ user }))
+        .then(user => {
+            this.setState({
+                user: user,
+                producer: user.producer,
+                minSell: user.minSell,
+                maxBuy: user.maxBuy
+            });
+        })
         .catch(window.alert);
     }
-
-    handleChange = ({target: { value }}) => {
-        this.setState({ username: value });
-    };
-
-    handleSubmit = () => {
-        const {
-            auth: { accessToken }
-        } = this.props;
-        const { user, username } = this.state;
-
-        this.setState({ loading: true });
-        if (!user) {
-            window.alert(
-                'The user id has not been fetched yet. Please try again in 5 seconds.'
-            );
-            return;
-        }
-
-        fetch(REACT_APP_BACKEND_URL+"/users/"+user._id, {
-            body: JSON.stringify({ username }),
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-                'Content-Type': 'application/json'
-            },
-            method: 'PATCH'
-            })
-            .then(response => response.json())
-            .then(user => this.setState({ loading: false, user }))
-            .catch(err => {
-                window.alert(err);
-                this.setState({ loading: false });
-        });
-    };
 
     render() {
         const {
@@ -82,11 +65,13 @@ export default class Wallet extends React.Component {
         return (
             <WalletView 
                 loading={loading}
-                handleChange={this.handleChange}
-                handleSubmit={this.handleSubmit}
                 onLoggedOut={onLoggedOut}
                 publicAddress={publicAddress}
                 username={username}
+                updateMinSell={this.updateMinSell}
+                minSell={this.state.minSell}
+                maxBuy={this.state.maxBuy}
+                producer={this.state.producer}
             />
         );
     }
--- frontend/src/views/Wallet/WalletView.js
+++ frontend/src/views/Wallet/WalletView.js
@@ -26,7 +26,7 @@ export default class WalletView extends React.Component {
                                     <div className="buy-price-toggle">
                                         <p className="toggle-title">BUY PRICE</p>
                                         <div className="toggle-display">
-                                            <p className="toggle-input">.08 ETH</p>
+                                            <p className="toggle-input">{this.props.maxBuy} ETH</p>
                                             <div className="adjust-button">
                                                 <p className="adjust-buy-button-text">ADJUST</p>
                                             </div>
@@ -36,16 +36,30 @@ export default class WalletView extends React.Component {
                                 </div>
                                 <div className="primary-col">
                                     <p className="wallet-subtitle">For Sellers</p>
-                                    <div className="sell-price-toggle">
-                                        <p className="toggle-title">SELL PRICE</p>
-                                        <div className="toggle-display">
-                                            <p className="toggle-input">.125 ETH</p>
-                                            <div className="adjust-button">
-                                                <p className="adjust-sell-button-text">ADJUST</p>
+                                    {
+                                        this.props.producer ?
+                                        <div className="sell-price-toggle">
+                                            <p className="toggle-title">SELL PRICE</p>
+                                            <div className="toggle-display">
+                                                <p className="toggle-input">{this.props.minSell} ETH</p>
+                                                <div className="adjust-button">
+                                                    <p className="adjust-sell-button-text">ADJUST</p>
+                                                </div>
                                             </div>
+                                            <p className="toggle-info">This is your minimum price per share (1 kWh). You will only sell energy shares at this price or above.</p>
+                                        </div> :
+                                        <div className="sell-price-toggle" style={{opacity: 0.5}}>
+                                            <p className="toggle-title">SELL PRICE</p>
+                                            <div className="toggle-display">
+                                                <p className="toggle-input">N/A</p>
+                                                <div className="adjust-button">
+                                                    <p className="adjust-sell-button-text">ADJUST</p>
+                                                </div>
+                                            </div>
+                                            <p className="toggle-info">This is your minimum price per share (1 kWh). You will only sell energy shares at this price or above.</p>
                                         </div>
-                                        <p className="toggle-info">This is your minimum price per share (1 kWh). You will only sell energy shares at this price or above.</p>
-                                    </div>
+                                    }
+                                    
                                 </div>
                                 <div className="secondary-col">
                                     <p className="wallet-subtitle">New Purchases</p>
@@ -55,7 +69,7 @@ export default class WalletView extends React.Component {
                                     <div className="match-box">
                                         <p className="match-box-vendor">Vendor:</p>
                                         <p className="match-box-vendor-value">0x5678909876545678987</p>
-                                        <p className="match-box-display">.28 ETH</p>
+                                        <p className="match-box-display">0.5 ETH</p>
                                         <div className="match-box-text-row">
                                             <p className="match-box-text"><b>Shares</b>: 4</p>
                                             <p className="match-box-text"><b>Price</b>: .07 ETH</p>
